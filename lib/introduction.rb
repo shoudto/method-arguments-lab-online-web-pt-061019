6bd794339106d5b0a1cff28b978aab959e443716
Oscar Shoudt <shoudto@gmail.com>
2019-06-27 16:14:54 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,3 @@ introduction(name)
   puts "Hi, my name is #{name}."
 end 
 
-introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{lanuguage}."
-end 
